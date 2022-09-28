b2b95d7495c5fd9e2f9bbbb390aac86e899d7b96
Karim Safiullin <st.herbinar@gmail.com>
2022-09-28 18:37:52 +0200

    remove commented out code

--- data_engineer_pet_project/transformers/join_bike_weather_datasets.py
+++ data_engineer_pet_project/transformers/join_bike_weather_datasets.py
@@ -2,11 +2,7 @@ from pyspark.sql import DataFrame, functions as f
 
 
 def join_weather_bike_datasets_job(
-        weather_df: DataFrame, bike_df: DataFrame,
-        # weather_name: str, weather_datetime: str, weather_temp: str,
-        # weather_precip: str, weather_snow: str, weather_wind_speed: str,
-        # bike_rideable_type: str, bike_started_at: str, bike_ended_at,
-        # bike_start_lat, bike_start_lng, bike_end_lat, bike_end_lng, bike_member_casual: str,
+        weather_df: DataFrame, bike_df: DataFrame
 ):
     """Join weather and bike trip dataset"""
     return bike_df.join(
